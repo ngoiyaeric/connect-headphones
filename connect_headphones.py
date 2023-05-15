caae10c78156344fa7af8e7924398c56099321e4
Eric Ngoiya <115367894+ngoiyaeric@users.noreply.github.com>
2023-05-15 13:20:06 +0300

    Add files via upload

--- connect_headphones.py
+++ connect_headphones.py
@@ -1,9 +1,5 @@
 import time
-import os
 import subprocess
-
-# Define the path to the shared directory
-shared_dir = "/path/to/shared_dir"
 
 def get_paired_devices():
     # Use the blueutil command to get a list of paired devices
@@ -15,28 +11,26 @@ def connect_headphones(mac_address):
     # Use the blueutil command to connect a specific device by MAC address
     subprocess.run(["blueutil", "--connect", mac_address], check=True)
 
+def is_bluetooth_on():
+    # Use the blueutil command to check if Bluetooth is on
+    output = subprocess.run(["blueutil", "--power"], capture_output=True, text=True)
+    return output.stdout.strip() == "1"
+
 def check_headphones():
     state = "OFF"  # Initial state
 
     while True:
-        if state == "OFF":
-            # Check if the headphones are connected
-            if os.path.exists(os.path.join(shared_dir, "headphones_connected")):
+        bluetooth_on = is_bluetooth_on()
+
+        if state == "OFF" and bluetooth_on:
+            devices = get_paired_devices()
+            if devices:
+                most_recent_device = devices[-1]
+                connect_headphones(most_recent_device)
                 state = "CONNECTED"
 
-        elif state == "DISCONNECTED":
-            # Check if the headphones are disconnected
-            if not os.path.exists(os.path.join(shared_dir, "headphones_connected")):
-                devices = get_paired_devices()
-                if devices:
-                    most_recent_device = devices[-1]
-                    connect_headphones(most_recent_device)
-                    state = "CONNECTED"
-
-        elif state == "CONNECTED":
-            # Check if the headphones are disconnected
-            if not os.path.exists(os.path.join(shared_dir, "headphones_connected")):
-                state = "DISCONNECTED"
+        elif state == "CONNECTED" and (not bluetooth_on or not get_paired_devices()):
+            state = "OFF"
 
         time.sleep(1)
 
