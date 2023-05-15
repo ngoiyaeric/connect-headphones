cfaabee600588207b643d151da9476ad1199db7d
Eric Ngoiya <115367894+ngoiyaeric@users.noreply.github.com>
2023-05-15 14:20:51 +0300

    Delete dockerconfig.bash

--- dockerconfig.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Build the Docker image
-docker build -t headphones-connector .
-
-# Run the Docker container with the shared volume
-docker run -v /path/to/shared_dir:/app/shared_dir headphones-connector
